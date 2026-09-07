b51a4ca36c53c5cb8f77d83ed55e16989cd59f02
Pablo <palodo2@alumni.uv.es>
2026-09-07 14:27:49 +0200

    Revert "El rediseño aprobado, llevado a la app"
    This reverts commit 57d9918304899f96b988652f72c799a56f2b5b4a.

--- fantasy-web/src/screens/Home.tsx
+++ fantasy-web/src/screens/Home.tsx
@@ -61,8 +61,7 @@ export default function Home({ me, onOpen, onLogout, invitacion, onInvitacionUsa
               </Empty>
             )}
             {leagues?.map((l) => (
-              <button key={l.id} onClick={() => onOpen(l.id)}
-                className={"leaguecard" + (l.market_open ? " leaguecard--live" : "")}>
+              <button key={l.id} className="leaguecard" onClick={() => onOpen(l.id)}>
                 <div className="leaguecard__top">
                   <span className="mono">{monogram(l.name)}</span>
                   <span className="leaguecard__id">
